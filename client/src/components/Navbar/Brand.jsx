49ce6cc046e168caaa7d67068c56b9d20153c3e2
Diablobait <thor.stg@gmail.com>
2020-05-23 21:39:18 +0200

    🔗 Brand link to home

--- client/src/components/Navbar/Brand.jsx
+++ client/src/components/Navbar/Brand.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Image } from "./Brand.styles";
 import logo from "./../../assets/images/logo.png";
 
-export default React.memo(() => <Image src={logo} alt="Company Logo" />);
+export default React.memo(() => (
+  <Link to="/">
+    <Image src={logo} alt="Company Logo" />
+  </Link>
+));
